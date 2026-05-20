294c13a658c7f222c0c23a3bfcb63ee61bb32a0a
Goldbro233 <bowensun_06@outlook.com>
2026-05-20 18:55:03 +0800

    Add floral-notepaper package maintenance

--- tests/test_repo_layout.sh
+++ tests/test_repo_layout.sh
@@ -31,9 +31,12 @@ assert_contains() {
 
 assert_file "packages/aio-coding-hub-bin/PKGBUILD"
 assert_file "packages/aio-coding-hub-bin/.SRCINFO"
+assert_file "packages/floral-notepaper/PKGBUILD"
+assert_file "packages/floral-notepaper/.SRCINFO"
 assert_file "packages/zigfetch/PKGBUILD"
 assert_file "packages/zigfetch/.SRCINFO"
 assert_file ".github/workflows/check-aio-coding-hub-bin-upstream-release.yml"
+assert_file ".github/workflows/check-floral-notepaper-upstream-release.yml"
 assert_file ".github/workflows/check-zigfetch-upstream-release.yml"
 assert_file "scripts/package-release.sh"
 
@@ -41,6 +44,8 @@ assert_not_file "PKGBUILD"
 assert_not_file ".SRCINFO"
 
 assert_contains ".github/workflows/check-aio-coding-hub-bin-upstream-release.yml" "PACKAGE_DIR: packages/aio-coding-hub-bin"
+assert_contains ".github/workflows/check-floral-notepaper-upstream-release.yml" "PACKAGE_DIR: packages/floral-notepaper"
 assert_contains ".github/workflows/check-zigfetch-upstream-release.yml" "PACKAGE_DIR: packages/zigfetch"
 assert_contains ".github/workflows/check-aio-coding-hub-bin-upstream-release.yml" "AUR_REMOTE_URL: ssh://aur@aur.archlinux.org/aio-coding-hub-bin.git"
+assert_contains ".github/workflows/check-floral-notepaper-upstream-release.yml" "AUR_REMOTE_URL: ssh://aur@aur.archlinux.org/floral-notepaper.git"
 assert_contains ".github/workflows/check-zigfetch-upstream-release.yml" "AUR_REMOTE_URL: ssh://aur@aur.archlinux.org/zigfetch.git"
